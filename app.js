ec8ccf18245949be24274a9144adf5bc98d9d071
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-08-17 04:36:15 +0900

    dot install #04

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ $(function(){
 		defaults: {
 			title: 'do it',
 			complited: false,
+		},
+		toggle: function () {
+			this.set('complited', !this.get('completed'));
 		}
+
 	});
 
 	var task1 = new Task();
+	// task1.set('title', 'newTitle');
+	// var title = task1.get('title');
+
+	//console.log(title);
+	// console.log(task1.toJSON());
 
 	console.log(task1.toJSON());
-  
+	task1.toggle();
+	console.log(task1.toJSON());
+
 });
